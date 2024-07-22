1c3d3f071b5ba181becdec991e568bfd232d2de7
KristianaTR <kristiana.tatarcuka@gmail.com>
2024-07-23 00:22:50 +0300

    create header

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,1 +1,18 @@
 import './Header.style.scss';
+import Button from '../Button';
+
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Product List</h1>
+        <div>
+            <Button text='ADD'/>
+            <Button text='MASS DELETE'/>
+        </div>
+    </header>
+  )
+}
+
+export default Header

3b39482f8cc851f343ba8c3e2ca8a489bef259cf
KristianaTR <kristiana.tatarcuka@gmail.com>
2024-07-23 17:42:13 +0300

    product list item

---
+++ frontend/src/Components/ProductListItem/ProductListItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductListItem = () => {
+  return (
+    <div className="product-item__container">
+        
+    </div>
+  )
+}
+
+export default ProductListItem

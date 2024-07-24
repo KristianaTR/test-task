7bbdd2068da5a3cf3a30a72714028d6fb4b890c6
KristianaTR <kristiana.tatarcuka@gmail.com>
2024-07-24 13:11:57 +0300

    product list item hardcoded version

--- frontend/src/Components/ProductListItem/ProductListItem.js
+++ frontend/src/Components/ProductListItem/ProductListItem.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import './ProductListItem.style.scss';
 
-const ProductListItem = () => {
+const ProductListItem = ({ product }) => {
   return (
     <div className="product-item__container">
-        
+      <input type="checkbox" className="delete-checkbox"></input>
+      <div className="product-item__details">
+        <p>{product.sku}</p>
+        <p>{product.name}</p>
+        <p>{product.price.toFixed(2)} $</p>
+        <p>
+          {(product.type === "Book") && `Weight: ${product.attribute.weight}`}
+          {(product.type === "DVD-disc") && `Size: ${product.attribute.size}`}
+          {(product.type === "Furniture") &&
+            `Dimensions: ${product.attribute.dimensions}`}
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductListItem
+export default ProductListItem;
---
+++ frontend/src/productData.js
@@ -0,0 +1,94 @@
+const productList = [
+  {
+    sku: "SKU001",
+    name: "The Great Gatsby",
+    price: 15.99,
+    type: "Book",
+    attribute: {
+      weight: "0.5 Kg",
+    },
+  },
+  {
+    sku: "SKU002",
+    name: "Inception",
+    price: 12.99,
+    type: "DVD-disc",
+    attribute: {
+      size: "700 MB",
+    },
+  },
+  {
+    sku: "SKU003",
+    name: "Modern Sofa",
+    price: 299.99,
+    type: "Furniture",
+    attribute: {
+      dimensions: "85x200x90 cm",
+    },
+  },
+  {
+    sku: "SKU004",
+    name: "To Kill a Mockingbird",
+    price: 10.99,
+    type: "Book",
+    attribute: {
+      weight: "0.6 Kg",
+    },
+  },
+  {
+    sku: "SKU005",
+    name: "The Matrix",
+    price: 14.99,
+    type: "DVD-disc",
+    attribute: {
+      size: "800 MB",
+    },
+  },
+  {
+    sku: "SKU006",
+    name: "Elegant Chair",
+    price: 150.0,
+    type: "Furniture",
+    attribute: {
+      dimensions: "100x50x60 cm",
+    },
+  },
+  {
+    sku: "SKU007",
+    name: "1984",
+    price: 9.99,
+    type: "Book",
+    attribute: {
+      weight: "0.4 Kg",
+    },
+  },
+  {
+    sku: "SKU008",
+    name: "Avatar",
+    price: 13.99,
+    type: "DVD-disc",
+    attribute: {
+      size: "650 MB",
+    },
+  },
+  {
+    sku: "SKU009",
+    name: "Dining Table",
+    price: 499.99,
+    type: "Furniture",
+    attribute: {
+      dimensions: "75x150x80 cm",
+    },
+  },
+  {
+    sku: "SKU010",
+    name: "Brave New World",
+    price: 11.99,
+    type: "Book",
+    attribute: {
+      weight: "0.7 Kg",
+    },
+  },
+];
+
+export default productList
